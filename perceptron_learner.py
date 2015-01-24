aef571499ae51d75c2b3b9baf569f6e6d31cd7db
Jarom McDonald <jlmcdonald@gmail.com>
2015-01-23 18:02:25 -0700

    Accuracy only at 70% ... I suspect we have to only add in bias nodes when we're running the epoch (not globally) as it might affect how we measure our accuracy.

--- perceptron_learner.py
+++ perceptron_learner.py
@@ -3,47 +3,50 @@ from supervised_learner import SupervisedLearner
 import random
 
 class PerceptronLearner(SupervisedLearner):
-    traininginputs, testinputs, weights = [],[],[]
-    trainingepoch,unprogressiveEpochs,epochLimit = 0,0,100
+    inputs,weights = [],[]
+    trainingepoch,unprogressiveEpochs,epochLimit = 0,0,500
     learningrate=.1
     currentAccuracy=0.0
-    reserve_training=True
- 
 
     def train(self, featurematrix, targetmatrix, initialized=False):
       if not initialized:
         self.initInputs(featurematrix,targetmatrix)
-      for idx,vector in enumerate(self.traininginputs):
+      while self.currentAccuracy!=1.0 and self.unprogressiveEpochs!=25 and self.trainingepoch!=self.epochLimit:
+        oldaccuracy=self.currentAccuracy
+        self.currentAccuracy=runEpoch(targetmatrix)
+        self.unprogressiveEpochs=0 if self.currentAccuracy > oldaccuracy else self.unprogressiveEpochs+1
+      self.removeBias()
+      print("Total epochs run: "+str(self.trainingepoch))
+
+    def runEpoch(self,targetmatrix):
+      self.trainingepoch+=1
+      for idx,vector in enumerate(self.inputs):
         output=self.checkVector(vector)
         if output!=int(targetmatrix.data[idx][0]):
           self.changeWeights(targetmatrix.data[idx][0],output,vector)
-      accuracy=self.measure_accuracy(featurematrix,targetmatrix)
-      if accuracy!=1.0 and self.unprogressiveEpochs!=25 and self.trainingepoch!=self.epochLimit:
-        self.trainingepoch+=1
-        self.unprogressiveEpochs=0 if accuracy>self.currentAccuracy else self.unprogressiveEpochs+1
-        self.currentAccuracy=accuracy
-        self.train(featurematrix,targetmatrix,initialized=True)
-      else:
-        print("Total epochs run: "+str(self.trainingepoch))
+      return self.measure_accuracy(featurematrix,targetmatrix)
 
     def predict(self, features, targets):
       targets += [self.checkVector(features)]
-      
-    def addBias(self,inputvector):
-      inputvector.insert(0,1)
-      return inputvector
-      
+
+    def addBias(self,features):
+      for inputvector in features:
+        inputvector.insert(0,1)
+      return features
+
+    def removeBias(self):
+      for inputvector in self.inputs:
+        inputvector.pop(0)
+      self.weights.pop(0)
+
     def initInputs(self,featurematrix,targetmatrix):
-      featurematrix.shuffle(targetmatrix)
-      delimiter=int(len(featurematrix.data)*.7)
-      self.traininginputs=[self.addBias(inputvector) for inputvector in featurematrix.data[:delimiter]]
-      self.testinputs=[self.addBias(inputvector) for inputvector in featurematrix.data[delimiter:]]
-      self.weights=[round(random.uniform(-1,1),1) for val in self.traininginputs[0]]
+      self.inputs=self.addBias([inputvector for inputvector in featurematrix.data])
+      self.weights=[round(random.uniform(-1,1),1) for val in self.inputs[0]]
 
     def changeWeights(self,target,output,inputvector):
       change=[self.learningrate*(int(target)-output)*inputvector[idx] for idx in range(len(self.weights))]
       self.weights=[round(self.weights[i]+change[i],2) for i in range(len(self.weights))]
-        
+
     def checkVector(self,vector):
       neuronValue=0
       for k,v in enumerate(vector):
